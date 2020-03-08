10d976258222d9dc2440c4c47881f7b3028b3176
Harihar Chowdhary <harihar2u@gmail.com>
2020-03-08 15:37:17 +0100

    Open Api and swagger ui integartion

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-This is sample project for Openshift diployment of Spring boot application with MySql persistance storage.
+###### Spring boot application with persistance storage.
+1. Openshift diployment
+2. Docker support
+3. OpenApi and Swagger UI integaration
+4. Spring security with JWT
+
+##### Open API and Swagger UI
+http://localhost:8081/estore-ws/swagger-ui.html
--- src/main/java/com/online/estore/api/ProductController.java
+++ src/main/java/com/online/estore/api/ProductController.java
@@ -3,22 +3,40 @@ package com.online.estore.api;
 import com.online.estore.model.Product;
 import com.online.estore.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.awt.*;
-import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/product")
 public class ProductController {
+
     @Autowired
     private ProductRepository productRepository;
 
-    @GetMapping
-    public Iterable<Product> getProducts() {
-        return productRepository.findAll();
+    @GetMapping(produces = "application/json")
+    public ResponseEntity<Page<Product>> getProducts(Pageable pageable) {
+        Page<Product> products = productRepository.findAll(pageable);
+        return new ResponseEntity<>(products, HttpStatus.OK);
+    }
+
+//    @GetMapping(produces = "application/json")
+//    public ResponseEntity<Product> getProduct(@PathVariable Long productId) {
+//        Optional product = productRepository.findById(productId);
+//        if (product.isPresent()) {
+//            return new ResponseEntity(product, HttpStatus.OK);
+//        } else {
+//            return new ResponseEntity(null, HttpStatus.NOT_FOUND);
+//        }
+//    }
+
+    @PostMapping(consumes = "application/json")
+    public ResponseEntity<Product> save(@RequestBody Product product) {
+        productRepository.save(product);
+
+        return ResponseEntity.accepted().body(product);
     }
 
 }
